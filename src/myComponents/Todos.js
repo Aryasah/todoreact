929cbf2b0b06a614a6a03091f74b080ba00dea9b
Arya Sah <75942764+Aryasah@users.noreply.github.com>
2021-05-12 18:31:41 +0530

    god

--- src/myComponents/Todos.js
+++ src/myComponents/Todos.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Todo from './Todo.js';
 import './Todos.css';
 export default function Todos(props) {
+    let style={
+    color:"#000",
+    }
     return (
         
         <div className="containers">
           
         <h3 className=" my-5 mb-5">ToDo's List</h3>
-        {props.todos.length==0?<h5 style={{color:red}}>no to dos to display</h5>:
+        {props.todos.length==0?<h5 style={style}>no to dos to display</h5>:
         props.todos.map((todo) =>{
              return(<Todo todo={todo} key={todo.s_n0} onDelete={props.onDelete}/>)
             
