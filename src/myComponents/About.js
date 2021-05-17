40c3b9e6d103a7980f9af43e8faf598b5ea23a5f
Arya Sah <75942764+Aryasah@users.noreply.github.com>
2021-05-17 21:21:52 +0530

    added social media link

--- src/myComponents/About.js
+++ src/myComponents/About.js
@@ -21,10 +21,10 @@ export default function About() {
             <div className="content">
                 <h2>Arya Sah<br/><span>Web Developer</span></h2>
                 <div className="social-icons">
-                    <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/arya.sah.98892"><i className="fa fa-facebook" aria-hidden="true"></i></a>
                     <a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-                    <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
-                    <a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/aryasah30/"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.linkedin.com/in/arya-sah-5100121b3/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
 
                 </div>
             </div>
